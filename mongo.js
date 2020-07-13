649e6443016c9f5891ee10b87bdc3fc26eff382e
shivani-save <shivani.save8@gmail.com>
2020-07-13 17:14:03 +0530

    3.12

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
+    console.log('Enterthe password as node mongo.js <password>')
     process.exit(1)
 }
 
@@ -18,12 +18,33 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-    name: 'Shivani',
-    number: '90047522'
-})
+// const person = new Person({
+//     name: 'Shivani',
+//     number: '90047522'
+// })
 
-person.save().then(result => {
-    console.log(`${name} saved to phonebook`)
-    mongoose.connection.close()
-})
+if (process.argv.length == 3) {
+    console.log("Phonebook:")
+
+    Person.find({}).then(results => {
+        results.forEach(result => {
+            console.log(`${result.name}: ${result.number}`)
+        })
+        mongoose.connection.close()
+    })
+
+} else if (process.argv.length == 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
