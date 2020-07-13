2b8936602d943e1f29c0b222a70ec933c87163c8
shivani-save <shivani.save8@gmail.com>
2020-07-13 20:23:21 +0530

    3.17

--- index.js
+++ index.js
@@ -115,6 +115,19 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const person = {
+      name: req.body.name,
+      number: req.body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+      .then(updatedPerson => {
+          res.json(updatedPerson)
+      })
+      .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   Person.findByIdAndRemove(req.params.id)
